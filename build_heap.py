4f99c798b9157b510c4e332f81e121c88a0ecf3e
Elina221RDB375 <125765070+Elina221RDB375@users.noreply.github.com>
2023-03-19 17:23:40 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -1,63 +1,44 @@
-def heap(info, i, n, swaps): 
-    minimums = i 
-    kreisais = 2 * i + 1 
-    labais = 2 * i + 2 
- 
-    if kreisais < n and info[kreisais] < info[minimums]: 
-        minimums = kreisais 
- 
-    if labais < n and info[labais] < info[minimums]: 
-        minimums = labais 
-    if i != minimums: 
-        info[i], info[minimums] = info[minimums], info[i] 
-        swaps.append((i, minimums)) 
-        heap(info, minimums, n, swaps) 
- 
 def build_heap(info): 
+    garums = len(info) 
     swaps = [] 
-    n = len(info) 
-     
-    for i in range(n // 2 - 1, -1, -1): 
-        heap(info, i, n, swaps) 
-     
+    for i in range(garums, -1, -1): 
+        j = i 
+        while True: 
+            zars = j*2 + 1 
+            if zars >= garums: 
+                break 
+            if zars+1 < garums and info[zars+1] < info[zars]: 
+                zars = zars+1 
+            if info[j] > info[zars]: 
+                swaps.append((j, zars)) 
+                info[j], info[zars] = info[zars], info[j] 
+                j = zars 
+            else: 
+                break 
     return swaps 
  
-def check(info, swaps): 
-    n = len(info) 
-    for i in range(n): 
-        for j in range(i + 1, n): 
-            if info[j] < info[i]: 
-                info[i], info[j] = info[j], info[i] 
-                swaps.append((i, j)) 
- 
-    return swaps 
  
 def main(): 
-    txt = input().strip() 
- 
-    if 'I' in txt: 
+    option = str(input()) 
+    if "I" in option: 
         n = int(input()) 
-        info = list(map(int, input().split())) 
+        data = list(map(int, input().split())) 
+        assert len(data) == n 
+        swaps = build_heap(data) 
+        print(len(swaps)) 
+        for i, j in swaps: 
+            print(i, j) 
+        return 
+    if "F" in option: 
+        file = str(input()) 
+        file = "tests/" + str(file) 
+        with open(file, 'r') as pakete: 
+            n = int(pakete.readline()) 
+            data = list(map(int, pakete.readline().split())) 
+        assert len(data) == n 
+        swaps = build_heap(data) 
+        print(len(swaps)) 
+        return    
  
-    if 'F' in txt: 
-        filename = input().strip() 
-        if 'a' not in filename: 
-            f_path = './tests/' + filename 
-            with open(f_path) as f: 
-                n = int(f.readline()) 
-                info = list(map(int, f.readline().split())) 
- 
- 
- 
-    assert len(info) == n 
- 
-    swaps = build_heap(info) 
-     
- 
-    print('swaps') 
-    print(len(swaps)) 
-    for i, j in swaps: 
-        print(i, j) 
- 
-if name == '__main__': 
+if name == "__main__": 
     main()
